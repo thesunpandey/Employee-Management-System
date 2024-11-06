791d8db78a15123e001cd400078ac5f14eaba738
Suraj Pandey <pandeyjisuraj15@gmail.com>
2024-11-06 16:50:19 +0530

    Updated App.jsx

--- employee-mang-system/src/App.jsx
+++ employee-mang-system/src/App.jsx
@@ -1,15 +1,11 @@
 /* eslint-disable */
-import { useState } from 'react'
-import './App.css'
-
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       
-    </>
+    </div>
   )
 }
 
